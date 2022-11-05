7fb0d0479278415da19bb857a06727ee55d0b877
Aatir Nadim <aatir.nadim@gmail.com>
2022-11-05 15:00:57 +0530

    starting off with routes

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,37 +1,20 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from './components/Login'
+import PhoneVerify from './components/PhoneVerify'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
-    <div>
-      <p>
-        hello from the frontend
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path = '/' element = {<Login/>}/>
+      <Route path = '/register' element = {<Signup/>}/>
+      <Route path = '/dashboard' element = {<Dashboard/>}/>
+      <Route path = '/phone/verify' element = {<PhoneVerify/>}/>
 
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
